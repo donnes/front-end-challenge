549d7a03f19d56046a203fdb45f90be823a62b15
Donald Silveira <donaldsilveira@gmail.com>
2021-08-18 20:56:48 -0300

    feat(mobile): Created useClients

---
+++ mobile/src/hooks/useClients.ts
@@ -0,0 +1,51 @@
+import {useSWRInfinite} from 'swr';
+import qs from 'qs';
+import {omit} from 'lodash';
+import {fetcher} from '@/lib';
+import {Client} from '@/types';
+
+type UseClientsReturn = {
+  results: Client[];
+  page: number;
+  isLoading: boolean;
+  isLoadingMore: boolean;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
+  setPage: (page: number) => void,
+};
+
+type UseClientsQuery = {
+  search: string;
+  gender: string;
+  page: string | number;
+  viewId: string;
+};
+
+const useClients = (query: UseClientsQuery): UseClientsReturn => {
+  const getKey = (pageIndex: number, previousPageData: any) => {
+    const fetchParams = omit(query, ['page', 'viewId', 'search']);
+    if (previousPageData && !previousPageData.length) return null;
+    return `/?results=50&page=${pageIndex}&${qs.stringify(fetchParams)}`;
+  }
+  const {data, error, isValidating, size: page, setSize: setPage} = useSWRInfinite(getKey, fetcher, {
+    revalidateOnFocus: false,
+  });
+  
+  const results = data ? [].concat(...data) : [];
+  const isLoading = !data && !error && isValidating;
+  const isLoadingMore = Boolean(isLoading || (page > 0 && data && typeof data[page - 1] === 'undefined'));
+  const isEmpty = Boolean(data?.[0]?.length === 0);
+  const isReachingEnd = Boolean(isEmpty || (data && data[data.length - 1]?.length < 50));
+
+  return {
+    results,
+    page,
+    isLoading,
+    isLoadingMore,
+    isEmpty,
+    isReachingEnd,
+    setPage,
+  }
+}
+
+export default useClients;

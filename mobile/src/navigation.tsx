786a7fd874a6a115df4ec49118d1c165a32f095a
Donald Silveira <donaldsilveira@gmail.com>
2021-08-19 15:17:59 -0300

    Added Client screen to stack navigation

--- mobile/src/navigation.tsx
+++ mobile/src/navigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './screens/home';
+import ClientScreen from './screens/client';
 
 import { RootStackParamList } from './types';
 
@@ -10,6 +11,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 const RootNavigator: React.FC = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Home" component={HomeScreen}/>
+    <Stack.Screen name="Client" component={ClientScreen}/>
   </Stack.Navigator>
 );
 
